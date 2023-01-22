a196be62c1ca275af949ed767b455c0d200f5d29
Cole Taylor <coletaylor017@gmail.com>
2023-01-22 14:34:30 -0700

    Empty activity commit

---
+++ app/src/main/java/com/cs4530/lifestyleapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cs4530.lifestyleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
