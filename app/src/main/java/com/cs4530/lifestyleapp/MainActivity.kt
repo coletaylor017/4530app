f54965a30124b28a0a39242aec7ee6ac6c0bcfbf
William <lazeralien@gmail.com>
2023-01-30 14:09:52 -0700

    Added camera functionality. TODO: Photo storage location, thumbnail location

--- app/src/main/java/com/cs4530/lifestyleapp/MainActivity.kt
+++ app/src/main/java/com/cs4530/lifestyleapp/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.cs4530.lifestyleapp
 
+import android.content.ActivityNotFoundException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.provider.MediaStore
+import android.graphics.Bitmap
+import android.os.Build
+import android.view.View
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+// Variables for UI elements
+    private var mButtonCamera: Button? = null
+
+    private var mIvPic: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Get the buttons
+        mButtonCamera = findViewById(R.id.PhotoButton)
+
+        //Say that this class itself contains the listener
+        mButtonCamera!!.setOnClickListener(this) //OnClickListener referenced in class declaration allows "this"
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) { //Added ? due to warning message. Consider better checks.
+
+            R.id.PhotoButton -> {
+                //The button press should open a camera
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                try {
+                    cameraActivity.launch(cameraIntent)
+                }catch(ex:ActivityNotFoundException){
+                    //Do error handling here
+                }
+            }
+        }
+    }
+    private val cameraActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        if(result.resultCode == RESULT_OK) {
+            mIvPic = findViewById<View>(R.id.iv_pic) as ImageView
+            //val extras = result.data!!.extras
+            //val thumbnailImage = extras!!["data"] as Bitmap?
+
+            if (Build.VERSION.SDK_INT >= 33) {
+                val thumbnailImage = result.data!!.getParcelableExtra("data", Bitmap::class.java)
+                mIvPic!!.setImageBitmap(thumbnailImage)
+            }
+            else {
+                val thumbnailImage = result.data!!.getParcelableExtra<Bitmap>("data")
+                mIvPic!!.setImageBitmap(thumbnailImage)
+            }
+        }
     }
 }
